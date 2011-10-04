89250e3b38a226d6717b5d3b3e77ec06c60796eb
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-10-04 12:12:14 -0500

    web: put redis init stuff insdie of configure

--- web.rb
+++ web.rb
@@ -1,6 +1,12 @@
 require 'redis'
 require 'sinatra'
 
+configure do
+  redis_url = ENV["REDISTOGO_URL"] || "redis://localhost:6379"
+  uri = URI.parse(redis_url)
+  set :redis, Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 get '/' do
   "<pre>curl -v https://sinatra-streaming-example.herokuapp.com/stream</pre>"
 end
@@ -8,12 +14,8 @@ end
 get '/stream' do
   puts "connection made"
 
-  redis_url = ENV["REDISTOGO_URL"] || "redis://localhost:6379"
-  uri = URI.parse(redis_url)
-  r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-
   stream do |out|
-    r.subscribe 'time' do |on|
+    settings.redis.subscribe 'time' do |on|
       on.message do |channel, message|
         out << "#{message}\n"
       end
